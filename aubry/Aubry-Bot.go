416ac7a6d9d29b206b2c9ac0466309c6a01a4f31
Aubry Arsenault <aubry@smarty.com>
2025-03-21 09:28:33 -0600

    changed something

--- aubry/Aubry-Bot.go
+++ aubry/Aubry-Bot.go
@@ -7,16 +7,16 @@ type Aubry struct {
 }
 
 func (this *Aubry) Play(game bank.GameInfo, yourInfo bank.PlayerInfo) {
-	if game.Round.Score >= 210 || game.Round.RoundNumber >= 17 || game.GetNumNOTBankedPlayers() == 1 {
+	if game.Round.Score >= 200 || game.Round.RoundNumber >= 18 || game.GetNumNOTBankedPlayers() == 1 {
 		this.Bank()
 	}
 }
 
-func (this *Aubry) LastChance(game bank.GameInfo, yourInfo bank.PlayerInfo) {
-}
-
-func (this *Aubry) RoundOver() {
-}
+//func (this *Aubry) LastChance(game bank.GameInfo, yourInfo bank.PlayerInfo) {
+//}
+//
+//func (this *Aubry) RoundOver() {
+//}
 
 func (this *Aubry) GetName() string {
 	return "Aubry's Special Bot"
